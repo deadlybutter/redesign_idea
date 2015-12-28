6311b2817f1f8fd5db3f6b7807086c0e67730fe9
deadlybutter <thedeadlybutter@gmail.com>
2015-12-27 22:25:20 -0500

    allows for env based port

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ request
     });
     campaignDataString = JSON.stringify(campaignData);
     console.log(campaignData);
-    var server = app.listen(3000, function () {
+    var server = app.listen(process.env.PORT || 3000, function () {
       var host = server.address().address;
       var port = server.address().port;
 
